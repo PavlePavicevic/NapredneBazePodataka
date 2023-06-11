71feb2540cc72eedf3f4ac2088f45a2c523c283b
PavlePavicevic <papaja@elfak.rs>
2023-06-11 23:21:48 +0200

    Razdvajanje zadataka

--- MyTravelExpAll/MyTravelExp/MyTravelExp/MyTravelExp/Models/Destination.cs
+++
@@ -1,8 +0,0 @@
-namespace Models
-{
-    public class Destination
-    {
-        public string ID { get; set; }
-        public string Name { get; set; }
-    }
-}
--- MyTravelExpAll/MyTravelExp/MyTravelExp/MyTravelExp/Models/Person.cs
+++
@@ -1,8 +0,0 @@
-namespace Models
-{
-    public class Person
-    {
-        public string ID { get; set; }
-        public string Username { get; set; }
-    }
-}
